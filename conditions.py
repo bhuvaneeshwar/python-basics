b4fc5c895e6197606cc0666624c91b77e8521975
Bhuvaneeshwar B <155140078+bhuvaneeshwar@users.noreply.github.com>
2025-12-29 18:55:27 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,25 @@
+#example 1
+age = 21
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+# Example 2
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Fail")
+# Example 3
+number = 7
+
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
